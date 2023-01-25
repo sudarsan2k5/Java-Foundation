57697ece83f79b205ab4b1103cf94303a892add2
Sudarsan Mansingh <sudarsan2k5@gmail.com>
2023-01-26 00:58:38 +0530

    add new file

---
+++ Arrays/min_elem_in_the_array.java
@@ -0,0 +1,12 @@
+
+//                                    Assignment Questions
+//Q4. Calculate the minimum element in the array {2, -3, 5, 8, 1, 0, -4} using standard library method
+//        for calculating the minimum element.
+//        Output:
+//        -4
+
+public class min_elem_in_the_array {
+    public static void main(String[] args) {
+
+    }
+}
